653d858a5c4e8ec731d33d6e830330c4e7f4a5cc
Ruxandra <filotiruxandra@gmail.com>
2025-05-19 19:04:33 +0300

    about me content

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/girl.jpg") #width=600
+
+with col2:
+    st.title("About Me")
+    content = """
+    Hey! My name is Adda and I am a Full Stack Developer, passionate about Python. This is my portfolio of web apps that I've created so far 😊. Here you can find the built-in apps and their source code. Enjoy!"""
+    st.info(content)
